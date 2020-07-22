96643c85c4a1550ba5a2148ed5c9def9211817f4
masahiro.kojima <mncc0220@gmail.com>
2020-07-22 12:49:36 +0000

    a

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -68,7 +68,7 @@ class LinebotController < ApplicationController
               min_per = 30
             case input
           # 明日
-            when /.*(明日|あした).*/
+          when /.*(明日|あした).*/
               per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
               per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
               per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
@@ -84,30 +84,30 @@ class LinebotController < ApplicationController
                   "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
               end
             # 明後日
-            when /.*(明後日|あさって).*/
-              per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
-              per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
-              per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
-              if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-            #雨
-              push =
-                  "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
-              else
-              # 晴
-              push =
-                  "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね"
-              end
-            # かわいいなど
-            when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
-  		      # ありがとう
-              push =
-                "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
-          # 	こんにちは
-            when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*
-  		    # こんにちは
-              push =
-                "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
-          # 適当なテキスト
+        #     when /.*(明後日|あさって).*/
+        #       per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
+        #       per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
+        #       per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
+        #       if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+        #     #雨
+        #       push =
+        #           "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
+        #       else
+        #       # 晴
+        #       push =
+        #           "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね"
+        #       end
+        #     # かわいいなど
+        #     when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
+  		    #   # ありがとう
+        #       push =
+        #         "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
+        #   # 	こんにちは
+        #     when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*
+  		    # # こんにちは
+        #       push =
+        #         "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+        #   # 適当なテキスト
           
         # 画像など
             end
